f0d4f18cd10d92cd3898517476c7d176f282c465
Raphael <phaelnogueira@outlook.com>
2022-07-04 17:41:34 -0300

    add react app prefix to api url enviroment variable

--- client/server.js
+++ client/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { resolve } = require("path");
 const dotenv = require("dotenv");
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 const port = process.env.PORT || 3001;
 
 app.use('/', express.static(resolve(__dirname, './build')));
