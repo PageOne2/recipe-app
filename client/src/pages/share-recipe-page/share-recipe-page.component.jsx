99c9f80b67411c87d66e050e8b73c8b27203590d
Raphael <phaelnogueira@outlook.com>
2022-07-03 23:15:44 -0300

    create form to share recipe

--- client/src/pages/share-recipe-page/share-recipe-page.component.jsx
+++ client/src/pages/share-recipe-page/share-recipe-page.component.jsx
@@ -1,12 +1,43 @@
+import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { v4 as uuidv4 } from "uuid";
 import { createRecipe } from "../../redux/redux-saga/sagaActions";
 
 import "./share-recipe-page.styles.css";
 
 const ShareRecipePage = () => {
+  const [recipeInfo, setRecipeInfo] = useState({
+    recipeName: '',
+    ingredients: [],
+    method: [],
+    preparationTime: 0
+  });
+  const ingredientInput = useRef(null);
+  const methodInput = useRef(null);
   const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const handleChange = (value, field) => {
+    if (field === 'recipeName') setRecipeInfo({ ...recipeInfo, recipeName: value });
+    if (field === 'time') setRecipeInfo({ ...recipeInfo, preparationTime: value });
+  }
+
+  const handleClick = (e, field) => {
+    e.preventDefault(e);
+    if (field === 'ingredient') {
+      setRecipeInfo(prev => ({ ...prev, ingredients: prev.ingredients.concat(ingredientInput.current.value) }));
+      setTimeout(() => ingredientInput.current.value = '', 200);
+    }
+    if (field === 'method') {
+      setRecipeInfo(prev => ({ ...prev, method: prev.method.concat(methodInput.current.value) }));
+      setTimeout(() => methodInput.current.value = '', 200);
+    }
+  }
 
   return (
     <div className="share-recipe-container">
@@ -14,43 +45,63 @@ const ShareRecipePage = () => {
         <div className="form-title">
           <h2>Share your recipe</h2>
         </div>
-        <Formik
-          initialValues={{
-            recipeName: '',
-            ingredients: [],
-            method: [],
-            preparationTime: 0
-          }}
-          validationSchema={Yup.object({
-            recipeName: Yup.string().required('A recipe must have a name'),
-            ingredients: Yup.array().of(Yup.string()).min(1, 'A recipe must have at least one ingredient').required('Required'),
-            method: Yup.array().of(Yup.string()).min(1, 'A recipe must have at least one preparation method').required('Required'),
-            preparationTime: Yup.number().min(1, 'Add a valid time').required('Required')
-          })}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            dispatch(createRecipe(values));
-            setSubmitting(false);
-            resetForm();
-          }}
-        >
-          <Form>
-            <label htmlFor="recipeName">Your recipe name</label>
-            <ErrorMessage name="recipeName" render={msg => <div className="error-msg">{msg}</div>}/>
-            <Field className="form-input" name="recipeName" type="text"/>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="input-container">
+            <div className="input-wrapper">
+              <label htmlFor="recipeName">Your recipe name</label>
+              <input className="share-input" name="recipeName" type="text" onChange={(e) => handleChange(e.target.value, 'recipeName')}/>
+            </div>
+          </div>
 
-            <label htmlFor="ingredients">Add your recipe ingredients</label>
-            <ErrorMessage name="ingredients" render={msg => <div className="error-msg">{msg}</div>}/>
-            <Field className="form-input" name="ingredients" type="text"/>
+          <div className="input-container">
+            <div className="input-wrapper">
+              <label htmlFor="ingredients">Add your recipe ingredients</label>
+              <input className="share-input" name="ingredients" type="text" ref={ingredientInput}/>
+            </div>
+            <div className="add-btn">
+              <button type="button" onClick={(e) => handleClick(e, 'ingredient')}>Add Ingredient</button>
+            </div>
+          </div>
 
-            <label htmlFor="method">Add preparation methods</label>
-            <ErrorMessage name="method" render={msg => <div className="error-msg">{msg}</div>}/>
-            <Field className="form-input" name="method" type="text"/>
+          <div className="input-container">
+            <div className="input-wrapper">
+              <label htmlFor="method">Add preparation methods</label>
+              <input className="share-input" name="method" type="text" ref={methodInput}/>
+            </div>
+            <div className="add-btn">
+              <button type="button" onClick={(e) => handleClick(e, 'method')}>Add Method</button>
+            </div>
+          </div>
 
-            <label htmlFor="preparationTime">Preparation time in minutes</label>
-            <ErrorMessage name="preparationTime" render={msg => <div className="error-msg">{msg}</div>}/>
-            <Field className="form-input" name="preparationTime" type="number"/>
-          </Form>
-        </Formik>
+          <div className="input-container">
+            <div className="input-wrapper">
+              <label htmlFor="preparationTime">Preparation time in minutes</label>
+              <input className="share-input" name="preparationTime" type="number" onChange={(e) => handleChange(e.target.value, 'time')}/>
+            </div>
+          </div>
+
+          <div className="submit-btn-wrapper"><button type="submit" className="submit-btn">Share Recipe</button></div>
+        </form>
+      </div>
+      <div className="recipeInfo">
+        <div><h3>Recipe Name: {recipeInfo.recipeName}</h3></div>
+        <div>
+          <h3>Ingredients:</h3>
+          <ul>
+            { recipeInfo.ingredients.map(item => (
+              <li key={uuidv4()}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>Methods:</h3>
+          <ol>
+            { recipeInfo.method.map(item => ( 
+              <li key={uuidv4()}>{item}</li>
+            ))}
+          </ol>
+        </div>
+        <div><h3>Preparation Time: {recipeInfo.preparationTime} min</h3></div>
       </div>
     </div>
   )
