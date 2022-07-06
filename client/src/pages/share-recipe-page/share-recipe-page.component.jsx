fa9c528d554d4e5251233e8576876e49fab7140b
Raphael <phaelnogueira@outlook.com>
2022-07-05 23:48:07 -0300

    add better styling to share-recipe-page

--- client/src/pages/share-recipe-page/share-recipe-page.component.jsx
+++ client/src/pages/share-recipe-page/share-recipe-page.component.jsx
@@ -4,6 +4,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { v4 as uuidv4 } from "uuid";
 import { createRecipe } from "../../redux/redux-saga/sagaActions";
+// import DeleteIcon from '@mui/icons-material/Delete';
 
 import "./share-recipe-page.styles.css";
 
@@ -14,6 +15,7 @@ const ShareRecipePage = () => {
     method: [],
     preparationTime: 0
   });
+  const recipeNameInput = useRef(null);
   const ingredientInput = useRef(null);
   const methodInput = useRef(null);
   const dispatch = useDispatch();
@@ -23,17 +25,18 @@ const ShareRecipePage = () => {
   }
 
   const handleChange = (value, field) => {
-    if (field === 'recipeName') setRecipeInfo({ ...recipeInfo, recipeName: value });
-    if (field === 'time') setRecipeInfo({ ...recipeInfo, preparationTime: value });
+    if (field === 'time') setRecipeInfo(prev => ({ ...prev, preparationTime: value }));
   }
 
   const handleClick = (e, field) => {
     e.preventDefault(e);
-    if (field === 'ingredient') {
+    if (field === 'recipeName') {
+      setRecipeInfo(prev => ({ ...prev, recipeName: recipeNameInput.current.value.toUpperCase() }));
+      setTimeout(() => recipeNameInput.current.value = '', 200);
+    } else if (field === 'ingredient') {
       setRecipeInfo(prev => ({ ...prev, ingredients: prev.ingredients.concat(ingredientInput.current.value) }));
       setTimeout(() => ingredientInput.current.value = '', 200);
-    }
-    if (field === 'method') {
+    } else if (field === 'method') {
       setRecipeInfo(prev => ({ ...prev, method: prev.method.concat(methodInput.current.value) }));
       setTimeout(() => methodInput.current.value = '', 200);
     }
@@ -41,7 +44,7 @@ const ShareRecipePage = () => {
 
   return (
     <div className="share-recipe-container">
-      <div className="form">
+      <div className="share-recipe-form">
         <div className="form-title">
           <h2>Share your recipe</h2>
         </div>
@@ -49,7 +52,10 @@ const ShareRecipePage = () => {
           <div className="input-container">
             <div className="input-wrapper">
               <label htmlFor="recipeName">Your recipe name</label>
-              <input className="share-input" name="recipeName" type="text" onChange={(e) => handleChange(e.target.value, 'recipeName')}/>
+              <input className="share-input" name="recipeName" type="text" ref={recipeNameInput}/>
+            </div>
+            <div className="add-btn">
+              <button type="button" onClick={(e) => handleClick(e, 'recipeName')}>Add Recipe Name</button>
             </div>
           </div>
 
@@ -83,25 +89,35 @@ const ShareRecipePage = () => {
           <div className="submit-btn-wrapper"><button type="submit" className="submit-btn">Share Recipe</button></div>
         </form>
       </div>
-      <div className="recipeInfo">
-        <div><h3>Recipe Name: {recipeInfo.recipeName}</h3></div>
-        <div>
-          <h3>Ingredients:</h3>
-          <ul>
+      <div className="recipe-info">
+        <div className="recipe-info-title recipe-name">
+          <h3>Recipe Name</h3>
+          {recipeInfo.recipeName.length ? <p>{recipeInfo.recipeName}</p> : null}
+        </div>
+        <div className="recipe-info-title ingredients">
+          <h3>Ingredients</h3>
+          <ul className="items-list">
             { recipeInfo.ingredients.map(item => (
               <li key={uuidv4()}>{item}</li>
             ))}
           </ul>
         </div>
-        <div>
-          <h3>Methods:</h3>
-          <ol>
+        <div className="recipe-info-title methods">
+          <h3>Methods</h3>
+          <ol className="items-list">
             { recipeInfo.method.map(item => ( 
-              <li key={uuidv4()}>{item}</li>
+              <div className="recipe-item">
+                <li key={uuidv4()}>{item}</li>
+                <div className="delete-item-btn">X</div>
+                {/* <DeleteIcon /> */}
+              </div>
             ))}
           </ol>
         </div>
-        <div><h3>Preparation Time: {recipeInfo.preparationTime} min</h3></div>
+        <div className="recipe-info-title preparation-time">
+          <h3>Preparation Time</h3>
+          <p>{recipeInfo.preparationTime} min</p>
+        </div>
       </div>
     </div>
   )
